2aa2a6a4a3ae2f2c6e299f1451d6fee37b9b7d59
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-05 10:37:59 +0530

    Update Navbar.jsx

--- 1m4uk6/src/Navbar/Navbar.jsx
+++ 1m4uk6/src/Navbar/Navbar.jsx
@@ -3,6 +3,10 @@ import HomeIcon from "@mui/icons-material/Home";
 import ConstructionIcon from "@mui/icons-material/Construction";
 import Vector from "..//Assets/Vector.png";
 import photo from "..//Assets/photo.png";
+import Rectangle1440 from "..//Assets/Rectangle1440.png";
+import image11 from "..//Assets/image11.png";
+import image9 from "..//Assets/image9.png";
+import Rectangle1442 from "..//Assets/Rectangle1440.png";
 export default function () {
   return (
     <div>
@@ -16,8 +20,10 @@ export default function () {
         <img src={ConstructionIcon} alt="" className="tools" />
         <img src={Vector} alt="" className="Vector" />
         <img src={photo} alt="" className="photo" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src={Rectangle1440} alt="" className="rect" />
+        <img src={image11} alt="" className="text" />
+        <img src={Rectangle1442} alt="" className="blank" />
+        <span></span>
       </div>
     </div>
   );
