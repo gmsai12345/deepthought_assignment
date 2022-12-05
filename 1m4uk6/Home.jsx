20db5489a1653f52b70d29226f25a60ade0f3e54
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-05 15:23:05 +0530

    Update Home.jsx

--- 1m4uk6/Home.jsx
+++ 1m4uk6/Home.jsx
@@ -6,6 +6,7 @@ import CommentIcon from "@mui/icons-material/Comment";
 import LinkIcon from "@mui/icons-material/Link";
 import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
 import SaveIcon from "@mui/icons-material/Save";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 export default function () {
   return (
     <div>
@@ -91,55 +92,79 @@ export default function () {
       </figure>
       4
       <figure data-title="Butter Cookies!">
+        <h1>Task Heading 4</h1>
         <figcaption>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop p
+          <embed
+            src="https://webhome.phy.duke.edu/~rgb/Class/intro_physics_1/intro_physics_1.pdf"
+            width="452"
+            height="612"
+          />
         </figcaption>
       </figure>
       5
-      <figure data-title="Butter Cookies!">
-        <figcaption>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop p
-        </figcaption>
+      <figure data-title="">
+        <h1>Task Heading 7</h1>
+        <h1>
+          <KeyboardArrowUpIcon /> Introduction
+        </h1>
+        <form action="/action_page.php">
+          <input type="text" id="fname" name="fname" />
+        </form>
+        <p>
+          <KeyboardArrowUpIcon /> Thread A
+        </p>
+        <figure data-title="Thread A">
+          <figcaption>
+            <figure data-title="Butter Cookies!">
+              <figcaption>
+                <form action="/action_page.php">
+                  <label for="fname">Example 1</label>
+                  <br />
+                  <input type="text" id="fname" name="fname" />
+                </form>
+              </figcaption>
+            </figure>
+            <p>+ Example</p>
+            <figure data-title="Butter Cookies!">
+              <figcaption>
+                <form action="/action_page.php">
+                  <label for="fname">Argument for Thread A</label>
+                  <br />
+                  <input type="text" id="fname" name="fname" />
+                </form>
+              </figcaption>
+              <p>+ New Thread</p>
+            </figure>
+          </figcaption>
+        </figure>
+        <h1>
+          <KeyboardArrowUpIcon /> Conclusion
+        </h1>
+        <form action="/action_page.php">
+          <input type="text" id="fname" name="fname" />
+        </form>
+
+        <br />
+        <SaveIcon />
+        <br />
+        <KeyboardArrowDownIcon />
       </figure>
       6
-      <figure data-title="Butter Cookies!">
-        <figcaption>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop p
-        </figcaption>
+      <figure>
+        <figcaption>Text Heading 8</figcaption>
+        <audio controls src="/media/cc0-audio/t-rex-roar.mp3">
+          <a href="/media/cc0-audio/t-rex-roar.mp3">Download audio</a>
+        </audio>
       </figure>
       7
       <figure data-title="Butter Cookies!">
-        <figcaption>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop p
-        </figcaption>
+        <figcaption>Task Heading 8</figcaption>
+        video controls width="250">
+        <iframe
+          src="https://www.youtube.com/embed/s8H0Sx7jy7c"
+          width="100px"
+          height="100px"
+        />
       </figure>
     </div>
   );
