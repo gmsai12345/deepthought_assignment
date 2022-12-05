f03415e9941e942c1dbefcf268de4447ba8afa16
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-05 12:11:06 +0530

    Update index.js

--- 1m4uk6/src/index.js
+++ 1m4uk6/src/index.js
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Navbar from "./Navbar/Navbar";
+import Home from "./Home/Home";
+import Sidebar from "./Sidebar/Sidebar";
 
-import App from "./App";
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <Navbar/>
+    <Sidebar/>
+    <Home/>
+    
+      </StrictMode>
 );
